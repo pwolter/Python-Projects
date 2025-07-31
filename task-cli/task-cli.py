b7ffc4f7dad74c95ec4650663391dd8a287a1c0d
Pablo Wolter <pwolter@gmail.com>
2025-07-30 18:13:26 -0700

    Moved json read to its own function

--- task-cli/task-cli.py
+++ task-cli/task-cli.py
@@ -16,31 +16,33 @@ def task_properties(task_id, description, status, createdAt, updatedAt):
     return properties
 
 
-def add_task(args):
+def read_tasks_file():
     tasks_list = []
-    counter = 0
+    try:
+        with open(tasks_file, "r") as openfile:
+            data_in = json.load(openfile)
+            for task in data_in:
+                tasks_list.append(task)
+        return tasks_list
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+        exit(1)
+
+
+def add_task(args):
 
     if Path(tasks_file).is_file():
-        try:
-            with open(tasks_file, "r") as openfile:
-                data_in = json.load(openfile)
-                for task in data_in:
-                    print(task)
-                    tasks_list.append(task)
-            print(data_in)
-            # tasks_list.append(data_in)
-            print(tasks_list)
-        except Exception as e:
-            print(f"An error has occurred: {e}")
-            exit(1)
+        tasks_list = read_tasks_file()
+        # try:
+        # with open(tasks_file, "r") as openfile:
+        # data_in = json.load(openfile)
+        # for task in data_in:
+        # tasks_list.append(task)
+        # except Exception as e:
+        # print(f"An error has occurred: {e}")
+        # exit(1)
 
         task_id = len(tasks_list) + 1
-        print("------------------------------------------")
-        print(task_id)
-        print(len(data_in))
-        print(data_in)
-        print(tasks_list)
-        print("------------------------------------------")
 
     if not Path(tasks_file).is_file():
         task_id = 1
@@ -60,11 +62,6 @@ def add_task(args):
 
     tasks_list.append(data_add)
 
-    print("++++++++++++++++++++++++++++++++++++++++++")
-    print(data_add, type(data_add))
-    print(tasks_list, type(tasks_list))
-    print("------------------------------------------")
-
     try:
         with open(tasks_file, "w") as f:
             json.dump(tasks_list, f)
@@ -73,13 +70,16 @@ def add_task(args):
         exit(1)
 
 
+def update_task(args):
+    print("Updating task")
+
+
 def list_tasks(args):
     if args.status == None:
         with open(tasks_file, "r") as openfile:
             data_in = json.load(openfile)
 
     print(data_in)
-    print(type(data_in))
 
 
 def main():
@@ -94,6 +94,11 @@ def main():
     parser_list_tasks = subparsers.add_parser("list", help="List tasks")
     parser_list_tasks.add_argument("--status", help="Task status")
     parser_list_tasks.set_defaults(funct=list_tasks)
+
+    parser_update_task = subparsers.add_parser("update", help="Update task")
+    parser_update_task.add_argument("task_id", help="Task id")
+    parser_update_task.add_argument("task_description", help="Task description")
+    parser_update_task.set_defaults(funct=update_task)
 
     args = parser.parse_args()
     args.funct(args)
